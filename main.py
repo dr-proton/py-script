53b8cceae8ff2e2affab1e4b09a48f6581bb0599
dr-proton <83426271+dr-proton@users.noreply.github.com>
2024-05-05 22:49:46 +0300

    Update main.py
    Change function print

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ for i in range(10, 254):
     result = subprocess.run(['ping', f"172.16.6.{i}", '-n', '1'], stdout=subprocess.PIPE)
     sleep(1)
     if result.returncode == 0:
-        print("Успешно")    
+        print("Successfully 172.16.6.{i}")    
     else:
-        print(f'Ошибка 172.16.6.{i} !')
+        print(f'unsuccessfully 172.16.6.{i} !')
 
